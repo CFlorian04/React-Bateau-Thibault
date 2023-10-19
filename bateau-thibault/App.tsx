088e72f82aa27aaa143bf46594743f9515a30676
CalvinMP <91535810+CalvinMP@users.noreply.github.com>
2023-10-19 17:01:49 +0200

    style

--- bateau-thibault/App.tsx
+++ bateau-thibault/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {ImageBackground, StyleSheet, Text, View, Button, SafeAreaView, Linking} from 'react-native';
 
-const image = {uri: '../assets/background.png'};
 
 const App = () => (
   <View style={styles.container}>
     <ImageBackground source={require("./assets/background.png")} resizeMode="cover" style={styles.image}>
-      <Text>Le bateau de Thibault</Text>
+      <Text style={styles.title}>Le bateau de Thibault</Text>
       
       <Text style={styles.getStartedText}>
           Vente en direct de notre bateau{"\n"}
@@ -14,9 +13,9 @@ const App = () => (
       </Text>
 
       <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-        <Text onPress={() => Linking.openURL("tel:+330663999978")}>Téléphone</Text>
-        <Text onPress={() => Linking.openURL("https://www.facebook.com/lebateaudethibault")}>Facebook</Text>
-        <Text onPress={() => Linking.openURL("mailto:lebateaudethibault@gmail.com")}>Contact</Text>
+        <Text style={styles.links} onPress={() => Linking.openURL("tel:+330663999978")}>Téléphone</Text>
+        <Text style={styles.links} onPress={() => Linking.openURL("https://www.facebook.com/lebateaudethibault")}>Facebook</Text>
+        <Text style={styles.links} onPress={() => Linking.openURL("mailto:lebateaudethibault@gmail.com")}>Contact</Text>
       </View>
 
       <SafeAreaView style={styles.container}>
@@ -37,10 +36,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  title: {
+    fontFamily: 'Satisfy',
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'white',
+  },
+
+  links: {
+    color: 'black',
+    textAlign: 'center'
+  },
+
   image: {
     flex: 1,
     justifyContent: 'center',
   },
+
   // Test uniquement
   text: {
     color: 'white',
@@ -50,19 +63,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     backgroundColor: '#000000c0',
   },
+
   // Grid test
   fixToText: {
     flex: 2,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+
   codeHighlightContainer: {
     borderRadius: 3,
     paddingHorizontal: 4,
   },
+
   homeScreenFilename: {
     marginVertical: 7,
   },
+  
   getStartedText: {
     fontSize: 17,
     lineHeight: 24,
