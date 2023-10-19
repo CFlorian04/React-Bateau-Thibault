57a5f661128088962b40d4cc78eb57e5e4229c78
CalvinMP <91535810+CalvinMP@users.noreply.github.com>
2023-10-19 22:13:37 +0200

    home page stylisée

--- bateau-thibault/App.tsx
+++ bateau-thibault/App.tsx
@@ -2,29 +2,60 @@ import React from 'react';
 import {ImageBackground, StyleSheet, Text, View, Button, SafeAreaView, Linking} from 'react-native';
 
 
+const Row = ({ children }) => (
+  <View style={styles.row}>{children}</View>
+)
+
+const Col = ({ numRows, children }) => {
+  return  (
+    <View style={styles[`${numRows}col`]}>{children}</View>
+  )
+}
+
 const App = () => (
-  <View style={styles.container}>
+  <View style={styles.main_container}>
     <ImageBackground source={require("./assets/background.png")} resizeMode="cover" style={styles.image}>
-      <Text style={styles.title}>Le bateau de Thibault</Text>
-      
-      <Text style={styles.getStartedText}>
-          Vente en direct de notre bateau{"\n"}
-          Produits selon la saison, Livraisons sur Paris{"\n"}
-      </Text>
 
-      <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-        <Text style={styles.links} onPress={() => Linking.openURL("tel:+330663999978")}>Téléphone</Text>
-        <Text style={styles.links} onPress={() => Linking.openURL("https://www.facebook.com/lebateaudethibault")}>Facebook</Text>
-        <Text style={styles.links} onPress={() => Linking.openURL("mailto:lebateaudethibault@gmail.com")}>Contact</Text>
-      </View>
+      <SafeAreaView style={styles.child_container}>
+        <Text style={styles.title}>Le bateau de Thibault</Text>
+        
+        <SafeAreaView style={styles.quarter}>
+          <Text style={styles.getStartedText}>
+              Vente en direct de notre bateau{"\n"}
+              Produits selon la saison, Livraisons sur Paris{"\n"}
+          </Text>
+          <Text style={styles.links} onPress={() => Linking.openURL("tel:+330663999978")}>Téléphone</Text>
+          <Text style={styles.links} onPress={() => Linking.openURL("https://www.facebook.com/lebateaudethibault")}>Facebook</Text>
+          <Text style={styles.links} onPress={() => Linking.openURL("mailto:lebateaudethibault@gmail.com")}>Contact</Text>
+        </SafeAreaView>
+      </SafeAreaView>
 
-      <SafeAreaView style={styles.container}>
-        <View style={styles.fixToText}>
-          <Button onPress={() => console.log("Clicked")} title="Produits et promotions" color="#2d2a2a42"/>
-          <Button title="Bateaux" color="#2d2a2a42"/>
-          <Button title="Restaurants" color="#2d2a2a42"/>
-          <Button title="Recettes" color="#2d2a2a42"/>
-          <Button title="Contact" color="#2d2a2a42"/>
+      <SafeAreaView style={styles.child_container}>
+        <View style={styles.buttons_container}>
+          <Row>
+            <Col numRows={2}>
+              <Button onPress={() => console.log("Clicked")} title="Produits et promotions" color="#2d2a2a42"/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col numRows={2}>
+              <Button title="Bateaux" color="#2d2a2a42"/>
+            </Col>
+
+            <Col numRows={2}>
+              <Button title="Restaurants" color="#2d2a2a42"/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col numRows={1}>
+              <Button title="Recettes" color="#2d2a2a42"/>
+            </Col>
+            <Col numRows={1}>
+              <Button title="Contact" color="#2d2a2a42"/>
+            </Col>
+          </Row>
         </View>
       </SafeAreaView>
 
@@ -33,15 +64,64 @@ const App = () => (
 );
 
 const styles = StyleSheet.create({
-  container: {
+  // toute la page
+  main_container: {
+    flex: 1,
+    // delete
+    // borderColor: 'red',
+    // borderWidth: 3,
+    // borderStyle: 'solid',
+  },
+
+
+  child_container: {
+    // delete
+    flex: 1,
+    // borderColor: 'blue',
+    // borderWidth: 3,
+    // borderStyle: 'solid',
+  },
+
+  quarter: {
+    flex: 1,
+
+    // borderColor: 'green',
+    // borderWidth: 3,
+    // borderStyle: 'solid',
+
+    paddingVertical: 50,
+  },
+
+  // begin testing
+  buttons_container: {
+    flex: 2,
+    marginHorizontal: 20,
+  },
+
+  row: {
+    flexDirection: 'row',
+  },
+
+  "1col": {
     flex: 1,
   },
+
+  "2col": {
+    flex: 2,
+  },
+
+  // end testing
 
   title: {
     fontFamily: 'Satisfy',
     fontSize: 24,
     textAlign: 'center',
     color: 'white',
+    paddingVertical: 50,
+
+    // borderColor: 'purple',
+    // borderWidth: 3,
+    // borderStyle: 'solid',
   },
 
   links: {
@@ -55,22 +135,14 @@ const styles = StyleSheet.create({
   },
 
   // Test uniquement
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
-  },
-
-  // Grid test
-  fixToText: {
-    flex: 2,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-
+  // text: {
+  //   color: 'white',
+  //   fontSize: 42,
+  //   lineHeight: 84,
+  //   fontWeight: 'bold',
+  //   textAlign: 'center',
+  //   backgroundColor: '#000000c0',
+  // },
   codeHighlightContainer: {
     borderRadius: 3,
     paddingHorizontal: 4,
@@ -81,6 +153,7 @@ const styles = StyleSheet.create({
   },
   
   getStartedText: {
+    // flex: 1,
     fontSize: 17,
     lineHeight: 24,
     textAlign: 'center',
---
+++ bateau-thibault/app/pages/contact.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, SafeAreaView, Linking} from 'react-native';
+
+
+const Contact = () => (
+    <View>Vide</View>
+
+  );
