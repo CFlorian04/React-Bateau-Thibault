102f8dc747bcaefe41122c4e09cb0c05bb3712e8
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2023-10-19 21:48:14 +0200

    Modification V2

--- bateau-thibault/screens/Vue3/vue3.tsx
+++ bateau-thibault/screens/Vue3/vue3.tsx
@@ -4,11 +4,11 @@ import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "./vue3.style";
 const RestPartun = () => {
+  const navigation = useNavigation();
     return (
       <ImageBackground
         source={require("../../Photo/background3.png")} // Replace with the path to your image
         style={styles.backgroundImage}
-        resizeMode="cover"
       >
         <View style={styles.container}>
           <Text style={styles.text}>Restaurantes partenaires</Text>
@@ -23,36 +23,54 @@ const RestPartun = () => {
         </View>
         <View style={styles.View_Butth}>
           <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity style={styles.Butt_BistGas}>
+            <TouchableOpacity
+              style={styles.Butt_BistGas}
+              onPress={() => navigation.navigate("vue30")}
+            >
               <View style={styles.view_text_Button}>
                 <Text style={styles.text_Button}>Bistrot des Gascons</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.Butt_BistGas2}>
+            <TouchableOpacity
+              style={styles.Butt_BistGas2}
+              onPress={() => navigation.navigate("vue31")}
+            >
               <View style={styles.view_text_Button}>
                 <Text style={styles.text_Button}>Les fous de l'ile</Text>
               </View>
             </TouchableOpacity>
           </View>
           <View style={{ flexDirection: "row", top: 10 }}>
-            <TouchableOpacity style={styles.Butt_BistGas}>
+            <TouchableOpacity
+              style={styles.Butt_BistGas}
+              onPress={() => navigation.navigate("vue32")}
+            >
               <View style={styles.view_text_Button}>
                 <Text style={styles.text_Button}>Bistrot Landais</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.Butt_BistGas2}>
+            <TouchableOpacity
+              style={styles.Butt_BistGas2}
+              onPress={() => navigation.navigate("vue33")}
+            >
               <View style={styles.view_text_Button}>
                 <Text style={styles.text_Button}>Villa 9-Trois</Text>
               </View>
             </TouchableOpacity>
           </View>
           <View style={{ flexDirection: "row", top: 20 }}>
-            <TouchableOpacity style={styles.Butt_BistGas}>
+            <TouchableOpacity
+              style={styles.Butt_BistGas}
+              onPress={() => navigation.navigate("vue34")}
+            >
               <View style={styles.view_text_Button}>
                 <Text style={styles.text_Button}>Bistrot du Sommelier</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.Butt_BistGas2}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("vue1")}
+              style={styles.Butt_BistGas2}
+            >
               <View style={styles.view_text_Button}>
                 <Text style={styles.text_Button}>Devenez partenaire!</Text>
               </View>
